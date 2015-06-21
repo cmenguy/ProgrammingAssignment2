a7cb4c2262ea4b2848079589cb4f4bf98be5362b
Charles Menguy <menguy.charles@gmail.com>
2015-06-21 08:20:18 -0400

    Implementation for makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## Wrapper around a matrix to provide functionality
+## to cache its inverse to save compute time
 makeCacheMatrix <- function(x = matrix()) {
-
+    # Input validation, just in case
+    if (!is.matrix(x)) {
+        stop("Invalid parameter, expected matrix")
+    }
+    
+    invertedmatrix <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        invertedmatrix <<- NULL
+    }
+    
+    # Functions for getting and setting cached inv. matrix value
+    get <- function() x
+    # Inverting the matrix using built-in solve() function in R
+    setinverse <- function(solve) invertedmatrix <<- solve
+    getinverse <- function() invertedmatrix
+    
+    list(
+        set = set, 
+        get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Returns the inverse of a matrix by either
+## computing it, or getting it from the cache
+## if it was already computed
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    invertedmatrix <- x$getinverse()
+    # If inverse available, return it to save time
+    if(!is.null(invertedmatrix)) {
+        message("Getting cached inverse matrix")
+        return(invertedmatrix)
+    }
+    # If inverse not available, compute it,
+    # cache it and return it
+    data <- x$get()
+    invertedmatrix <- solve(data)
+    x$setinverse(invertedmatrix)
+    invertedmatrix
 }
